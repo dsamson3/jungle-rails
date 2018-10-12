434147ad5d8475c74d54fc650f9f9081d3727c5a
Dave <dave.samson91@gmail.com>
2018-10-12 18:17:08 +0000

    validated user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-
     has_secure_password
+    validates :name, presence: true
+    validates :email, presence: true, :uniqueness => {:scope => :email}
     
+    validates :password_confirmation, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   get '/logout' => 'sessions#destroy'
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
+  get "/users" => "users#new"
 
   namespace :admin do
     root to: 'dashboard#show'
